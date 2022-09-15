f474807e11dd6e604f6c28f27fab1d309ec4943d
serhatYilmazz <serhat.yilmaz1@trendyol.com>
2022-09-15 16:34:14 +0300

    feat: add cli example to cli plugin
    Co-Authored-by: Erkan Zileli <erkan.zileli@trendyol.com> Co-Authored-by: Celal Öner <celal.oner@trendyol.com>

--- host/go.mod
+++ host/go.mod
@@ -1,13 +1,19 @@
 module github.com/trendyol/smurfs/host
 
-go 1.19
+go 1.18
+
+require (
+	github.com/spf13/cobra v1.5.0
+	google.golang.org/grpc v1.49.0
+	google.golang.org/protobuf v1.27.1
+)
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
 	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
 	golang.org/x/text v0.3.3 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	google.golang.org/grpc v1.49.0 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
 )
--- host/main.go
+++ host/main.go
@@ -2,24 +2,85 @@ package main
 
 import (
 	"fmt"
-	"github.com/trendyol/smurfs/host/internal/service"
+	"github.com/spf13/cobra"
 	"github.com/trendyol/smurfs/host/protos"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-	"log"
-	"net"
+	"os"
 )
 
+var rootCmd = &cobra.Command{
+	Use: "ty",
+}
+
+var commandManifests = []*protos.Command{
+	{
+		Name:        "login",
+		Description: "to login with LDAP credentials",
+		Flags: []*protos.CommandFlag {
+			{
+				Name: "email",
+				Required: true,
+				Repeated: false,
+				Description: "User email",
+			},
+			{
+				Name: "password",
+				Required: true,
+				Repeated: false,
+				Description: "User password",
+			},
+		},
+	},
+	{
+		Name:        "logout",
+		Description: "to logout from CLI",
+		Flags: []*protos.CommandFlag{
+			{
+				Name: "email",
+				Required: false,
+				Repeated: false,
+				Description: "User email",
+			},
+		},
+
+	},
+}
+
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:8080"))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	for _, command := range commandManifests {
+		subCommand := &cobra.Command{
+			Use: command.Name,
+			Run: func(cmd *cobra.Command, args []string) {
+				fmt.Println("Running the command " + command.Name)
+				for i := range args {
+					fmt.Println(i)
+				}
+			},
+			Short:   command.Description,
+			Example: command.Usage,
+		}
+		rootCmd.AddCommand(subCommand)
+		for _, flag := range command.Flags {
+			subCommand.Flags().StringP(flag.Name, flag.Short, "", flag.Description)
+			if flag.Required {
+				subCommand.MarkFlagRequired(flag.Name)
+			}
+		}
 	}
 
-	server := grpc.NewServer()
-	reflection.Register(server)
-	protos.RegisterLogServiceServer(server, service.NewLogService())
-	if err = server.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
+
+	//lis, err := net.Listen("tcp", fmt.Sprintf("localhost:8080"))
+	//if err != nil {
+	//	log.Fatalf("failed to listen: %v", err)
+	//}
+	//
+	//server := grpc.NewServer()
+	//reflection.Register(server)
+	//protos.RegisterLogServiceServer(server, service.NewLogService())
+	//if err = server.Serve(lis); err != nil {
+	//	log.Fatalf("failed to serve: %v", err)
+	//}
 }
