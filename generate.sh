c8c5db539fd0bf4c4da72f9ef3a9f1b7fb61b708
Baris Ceviz <baris.ceviz@trendyol.com>
2022-09-20 02:14:40 +0200

    feat(protos): change proto path and generate into go lib

--- generate.sh
+++ generate.sh
@@ -5,6 +5,6 @@ set -e
 PROTO_FILES=$(find . -type f -name "*.proto" | xargs)
 
 for proto_file in $PROTO_FILES ; do
-  protoc --go_out=host --go_out=client --go_opt=paths=source_relative --go-grpc_out=host --go-grpc_out=client --go-grpc_opt=paths=source_relative $proto_file
+  protoc --go_out=go --go_opt=paths=source_relative --go-grpc_out=go --go-grpc_opt=paths=source_relative $proto_file
   echo "Generated code for $proto_file"
 done
